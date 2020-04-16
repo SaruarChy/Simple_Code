f1c6cebd027237dca70e243db37c2fbcb57d67d0
Saruar Chowdhury <56188614+SaruarChy@users.noreply.github.com>
2020-04-17 01:33:28 +0600

    Add files via upload

---
+++ Find Odd Even by Division Operator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if((n/2)*2==n){
+        cout<<"Given number is Even\n";
+    }
+    else{
+        cout<<"Given number is Odd\n";
+    }
+    return 0;
+}
